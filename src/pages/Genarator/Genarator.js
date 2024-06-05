5c940fc8aac9ae16c2557259d11c731c6dc06732
thimira <thimirapanditha20@gmail.com>
2024-06-05 21:22:28 +0530

    v6

--- src/pages/Genarator/Genarator.js
+++ src/pages/Genarator/Genarator.js
@@ -3,69 +3,6 @@ import "./genarator.css";
 import QrCodeIcon from "@mui/icons-material/QrCode";
 
 function Genarator(props) {
-  const form = document.getElementById("generate-form");
-  const qr = document.getElementById("qrcode");
-
-  const onGenarateSubmit = (e) => {
-    e.preventDefault();
-    clearUI();
-    const url = document.getElementById("url").value;
-    const size = document.getElementById("size").value;
-
-    if (url === "") {
-      alert("Please enter a valid URL");
-    } else {
-      showSpinner();
-
-      setTimeout(() => {
-        hideSpinner();
-        generateQRCode(url, size);
-        setTimeout(() => {
-          const saveUrl = qr.querySelector("img").src;
-          createSaveBtn(saveUrl);
-        }, 50);
-      }, 1000);
-    }
-  };
-
-  const generateQRCode = (url, size) => {
-    const qrcode = new QRCode("qrcode", {
-      text: url,
-      width: size,
-      height: size,
-    });
-  };
-
-  const showSpinner = function () {
-    document.getElementById("spinner").style.display = "block";
-  };
-
-  const hideSpinner = function () {
-    document.getElementById("spinner").style.display = "none";
-  };
-
-  const clearUI = function () {
-    qr.innerHTML = "";
-    const saveBtn = document.getElementById("savelink");
-    if (saveBtn) {
-      saveBtn.remove();
-    }
-  };
-
-  const createSaveBtn = function (saveUrl) {
-    const link = document.createElement("a");
-    link.id = "savelink";
-    link.classList = "download-btn";
-    link.href = saveUrl;
-    link.download = "qrcode";
-    link.innerHTML = "Save Image";
-    document.getElementById("generated").appendChild(link);
-  };
-
-  hideSpinner();
-
-  form.addEventListener("submit", onGenarateSubmit);
-
   return (
     <div className="genaratorBox">
       <div className="genaratorBoxTop">
@@ -79,7 +16,7 @@ function Genarator(props) {
             <input placeholder="Paste your link here" className="qrInput" />
           </div>
           <div className="sizeBox">
-            <select class="selected" name="size" id="size">
+            <select class="selected" onS name="size" id="size">
               <option value="100">100x100</option>
               <option value="200">200x200</option>
               <option value="300" selected>
