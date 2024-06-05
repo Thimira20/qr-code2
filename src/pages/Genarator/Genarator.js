68093ae6767fecff974d1aabee12b1570936bdf0
thimira <thimirapanditha20@gmail.com>
2024-06-05 06:54:59 +0530

    v5.2

--- src/pages/Genarator/Genarator.js
+++ src/pages/Genarator/Genarator.js
@@ -3,6 +3,69 @@ import "./genarator.css";
 import QrCodeIcon from "@mui/icons-material/QrCode";
 
 function Genarator(props) {
+  const form = document.getElementById("generate-form");
+  const qr = document.getElementById("qrcode");
+
+  const onGenarateSubmit = (e) => {
+    e.preventDefault();
+    clearUI();
+    const url = document.getElementById("url").value;
+    const size = document.getElementById("size").value;
+
+    if (url === "") {
+      alert("Please enter a valid URL");
+    } else {
+      showSpinner();
+
+      setTimeout(() => {
+        hideSpinner();
+        generateQRCode(url, size);
+        setTimeout(() => {
+          const saveUrl = qr.querySelector("img").src;
+          createSaveBtn(saveUrl);
+        }, 50);
+      }, 1000);
+    }
+  };
+
+  const generateQRCode = (url, size) => {
+    const qrcode = new QRCode("qrcode", {
+      text: url,
+      width: size,
+      height: size,
+    });
+  };
+
+  const showSpinner = function () {
+    document.getElementById("spinner").style.display = "block";
+  };
+
+  const hideSpinner = function () {
+    document.getElementById("spinner").style.display = "none";
+  };
+
+  const clearUI = function () {
+    qr.innerHTML = "";
+    const saveBtn = document.getElementById("savelink");
+    if (saveBtn) {
+      saveBtn.remove();
+    }
+  };
+
+  const createSaveBtn = function (saveUrl) {
+    const link = document.createElement("a");
+    link.id = "savelink";
+    link.classList = "download-btn";
+    link.href = saveUrl;
+    link.download = "qrcode";
+    link.innerHTML = "Save Image";
+    document.getElementById("generated").appendChild(link);
+  };
+
+  hideSpinner();
+
+  form.addEventListener("submit", onGenarateSubmit);
+
   return (
     <div className="genaratorBox">
       <div className="genaratorBoxTop">
@@ -10,10 +73,26 @@ function Genarator(props) {
       </div>
       <div className="genaratorBoxUp">
         <div className="spacingBox"></div>
-        <div className="inputBox">
-          <QrCodeIcon className="qrIcon" />
-          <input placeholder="Paste your link here" className="qrInput" />
+        <div className="urlSizeBox">
+          <div className="inputBox">
+            <QrCodeIcon className="qrIcon" />
+            <input placeholder="Paste your link here" className="qrInput" />
+          </div>
+          <div className="sizeBox">
+            <select class="selected" name="size" id="size">
+              <option value="100">100x100</option>
+              <option value="200">200x200</option>
+              <option value="300" selected>
+                300x300
+              </option>
+              <option value="400">400x400</option>
+              <option value="500">500x500</option>
+              <option value="600">600x600</option>
+              <option value="700">700x700</option>
+            </select>
+          </div>
         </div>
+
         <div className="buttonBox">
           <button className="genButton">Genarate</button>
         </div>
@@ -21,11 +100,11 @@ function Genarator(props) {
       </div>
       <div className="genaratorBoxDown">
         <div className="qrBox">
-          <img src="/images/qr-codeHome.png" alt="" className="qrImage" />
+          <img src="" alt="" className="qrImage" />
         </div>
       </div>
       <div className="genaratorBoxBottom">
-        <button className="delButton">Download</button>
+        <button className="genButton">Download</button>
       </div>
     </div>
   );
